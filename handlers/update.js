3738d1a866d4f49c26b4492f02eff777bd96a18c
Ryan Yogan <ryanyogan@gmail.com>
2018-03-22 13:19:03 -0700

    added in update handler

---
+++ handlers/update.js
@@ -0,0 +1,26 @@
+import * as db from '../libs/dynamodb-lib';
+import { success, failure } from '../libs/response-lib';
+
+export const main = async (event, context, callback) => {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: 'notes',
+    Key: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      noteId: event.pathParameters.id,
+    },
+    UpdateExpression: 'SET content = :content, attachment = :attachment',
+    ExpressionAttributeValues: {
+      ':attachment': data.attachment ? data.attachment : null,
+      ':content': data.content ? data.content : null,
+    },
+    ReturnValues: 'ALL_NEW',
+  };
+
+  try {
+    await db.call('update', params);
+    callback(null, success({ status: true }));
+  } catch (error) {
+    callback(null, failure({ status: false }));
+  }
+};
